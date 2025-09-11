10d2994570eacfc33c618e80f337ebc4c6b8de05
Adam Krellenstein <adam@krellenstein.com>
2025-09-11 11:54:54 +0200

    Some Repo Cleanup; Tweak Language in Index

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Website
 
-This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+The Sigil documentation site is built using [Docusaurus](https://docusaurus.io/).
 
 ## Installation
 
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Sigil Development Group',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'A Next-Generation Framework for Smart Contracts on Bitcoin',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -99,56 +99,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: '/docs/getting-started',
-            },
-            {
-              label: 'Examples',
-              to: '/docs/examples',
-            },
-            {
-              label: 'Reference',
-              to: '/docs/category/reference',
-            }
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Unspendable Labs, Inc.`,
     },
     prism: {
       theme: prismThemes.github,
